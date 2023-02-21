ffa7550dd4059f1900adf26c2a3d7d0461d5895a
Rúben Miguel Oliveira Tadeia <rubent03@gmail.com>
2023-02-21 10:32:44 +0000

    Added new image

---
+++ Experimenting/Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("I'm Printing to the Console");
+        Debug.Log("Can't Kick this ball!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
